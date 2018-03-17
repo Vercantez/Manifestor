c21c0b44667393a1a9a69d1d662d55f8fe6709a8
Miguel Salinas <mikeys@me.com>
2018-03-17 13:28:21 -0500

    Initial Commit

---
+++ Manifestor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Manifestor
+//
+//  Created by Mikey Salinas on 3/17/18.
+//  Copyright © 2018 Miguel Salinas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
